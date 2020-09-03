c29f1bc4cf617b260dfbff1f55ba62cca1a9c6f1
EunDaeng <55231698+EunDaengED@users.noreply.github.com>
2020-09-03 19:46:42 +0900

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
-}).listen(80, '0.0.0.0');
-console.log('Server running at http://127.0.0.1:80/');
